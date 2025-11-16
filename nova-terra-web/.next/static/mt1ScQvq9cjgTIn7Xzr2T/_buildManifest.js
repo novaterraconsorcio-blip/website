508173b931f7b2f48983cbaaa148acc0da42d821
Orçamento X <orcamentoxis@gmail.com>
2025-11-16 11:39:06 -0300

    chore: remover app Next.js e funções locais; manter conexões (Supabase, Resend, GitHub)

--- nova-terra-web/.next/static/mt1ScQvq9cjgTIn7Xzr2T/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-68056200a1583d79.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
